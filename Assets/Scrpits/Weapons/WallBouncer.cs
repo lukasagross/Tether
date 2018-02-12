67c928e762418911fe2e9f6165a06288151eeb58
Simon <simon.lothschutz@gmail.com>
2018-02-11 18:10:07 -0600

    Added weapon controller and wallbouncer weapon
    The player now has a weaponcontroller component, which as the wallbouncer weapon as its child. The wallbouncer gives the player velocity opposite the direction of their control stick when it hits a wall.

---
+++ Assets/Scrpits/Weapons/WallBouncer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBouncer : MonoBehaviour {
+    private float timeElapsed;
+    private SpriteRenderer sr;
+    private float rotation;
+    private Transform pTransform;
+    private Collider2D col;
+    private Vector2 reboundDir;
+
+    void Awake()
+    {
+        timeElapsed = 35;
+        sr = GetComponent<SpriteRenderer>();
+        pTransform = transform.parent;
+        col = GetComponent<Collider2D>();
+        reboundDir = Vector2.zero;
+    }
+	
+	void Update () {
+        if(timeElapsed > .2f) //Duration of hitbox in seconds
+        {
+            sr.color = new Color(0, 0, 0, 0);
+            col.enabled = false;
+        }
+        timeElapsed += Time.deltaTime;
+	}
+
+    public void Attack(Vector2 aimDirection)
+    {
+        if (timeElapsed > .6f) //Recovery time between attacks in seconds
+        {
+            timeElapsed = 0;
+            col.enabled = true;
+            sr.color = new Color(1, 0.92f, 0.016f, 1); //yellow!!
+            reboundDir = aimDirection.normalized*-7f; //The player rebounds opposite the direction they are aiming
+            
+            rotation = Vector2.Angle(aimDirection, Vector2.up);
+            Vector3 tempCross = Vector3.Cross(aimDirection, Vector2.up);
+            if (tempCross.z > 0)
+            {
+                rotation = 360 - rotation;
+            }
+            pTransform.rotation = Quaternion.Euler(0, 0, rotation);
+        }
+    }
+
+    //Handle collisions
+   void OnTriggerEnter2D(Collider2D Other)
+    {
+        GameObject collidedObject = Other.gameObject;
+
+        if(collidedObject.GetComponent<WallBouncer>() != null)
+        {
+            GetComponentInParent<PlayerMovement>().AddVelocity(reboundDir);
+        }
+        else if(collidedObject.GetComponent<PlayerMovement>() != null)
+        {
+            //Add code for victory condition
+            Debug.Log("Defeat Opponent");
+        }else if (collidedObject.GetComponent<Obstacle>())
+        {
+            GetComponentInParent<PlayerMovement>().AddVelocity(reboundDir);
+        }
+
+    }
+
+}
---
+++ Assets/Scrpits/Weapons/WeaponController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+    private Vector2 aimDirection;
+    private float attackInput;
+    private float rotation;
+    private Transform weapon;
+    
+	
+	void Awake()
+    {
+        weapon = transform.GetChild(0);
+    }
+	
+	void Update () {
+        HandleInput();
+	}
+
+    private void HandleInput()
+    {
+        aimDirection = new Vector2(Input.GetAxis("Controller-Horizontal"), Input.GetAxis("Controller-Vertical"));
+        attackInput = Input.GetAxis("Controller-Attack");
+        
+        if(attackInput == 1f)
+        {
+            if (weapon.GetComponent<WallBouncer>() != null)
+            {
+                weapon.GetComponent<WallBouncer>().Attack(aimDirection);
+            }
+        }
+    }
+}
