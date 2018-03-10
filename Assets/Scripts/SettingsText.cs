af808f5976c8055900b1efe06545159afc5e2327
lukasagross <lukasgross2020@u.northwestern.edu>
2018-03-09 19:53:28 -0600

    More work

---
+++ Assets/Scripts/SettingsText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsText : MonoBehaviour {
+
+    private Text text;
+    public string basetext;
+
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	void Update () {
+
+    }
+
+    public void UpdateText(string newtext)
+    {
+        text.text = basetext;
+        IEnumerator addLetters = AddLetters(newtext);
+        StartCoroutine(addLetters);
+    }
+
+    IEnumerator AddLetters(string newtext)
+    {
+        bool skip = false;
+        foreach (char c in newtext)
+        {
+            
+            text.text += c;
+            yield return null;
+            yield return null;
+        }
+    }
+}
