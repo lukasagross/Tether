904fed4dc92d96d0bfb3dc2faf306ba12374b371
Simon <simon.lothschutz@gmail.com>
2018-03-11 16:58:25 -0500

    Bug fixes and resolution changes
    The player can no longer get under the moving platform on the hell caves level. Additionally, hell caves and underwater caves are both 16:9 now

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -44,4 +44,36 @@ public class Spikes : MonoBehaviour {
             
         }
     }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        PlayerHealth plh = col.gameObject.GetComponent<PlayerHealth>();
+        PlayerMovement plm = col.gameObject.GetComponent<PlayerMovement>();
+        PlayerControls plc = col.gameObject.GetComponent<PlayerControls>();
+        if (plh != null)
+        {
+            if (plh.canDamage())
+            {
+                plh.takeDamage();
+            }
+            sc.AddScore(plc.playerNum, -1);
+            if (d == Direction.up)
+            {
+                plm.SetVelocity(new Vector2(col.gameObject.GetComponent<Rigidbody2D>().velocity.x, 8));
+            }
+            else if (d == Direction.down)
+            {
+                plm.SetVelocity(new Vector2(col.gameObject.GetComponent<Rigidbody2D>().velocity.x, -8));
+            }
+            else if (d == Direction.left)
+            {
+                plm.SetVelocity(new Vector2(8, col.gameObject.GetComponent<Rigidbody2D>().velocity.y));
+            }
+            else
+            {
+                plm.SetVelocity(new Vector2(-8, col.gameObject.GetComponent<Rigidbody2D>().velocity.y));
+            }
+
+        }
+    }
 }
