03e4fdb7b8da5446cf5775a7ddbdbd37e8ea59d3
Simon <simon.lothschutz@gmail.com>
2018-03-11 20:50:25 -0500

    added active carrot gamemode variable

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameMode : MonoBehaviour {
 
     private Score sc;
+    private CarrotController cc;
 
     public enum Mode {hits, health, carrots}
     public Mode currentMode;
@@ -14,6 +15,7 @@ public class GameMode : MonoBehaviour {
     void Awake()
     {
         sc = FindObjectOfType<Score>();
+        cc = FindObjectOfType<CarrotController>();
         StartCoroutine(StartUp());
     }
 
@@ -25,14 +27,20 @@ public class GameMode : MonoBehaviour {
             switch (currentMode)
         {
             case (Mode.hits):
+                cc.active = false;
+
                 break;
             case (Mode.health):
+                cc.active = false;
+
                 sc.AddScore(1, 10);
                 sc.AddScore(2, 10);
                 sc.AddScore(3, 10);
                 sc.AddScore(4, 10);
                 break;
             case (Mode.carrots):
+                cc.active = true;
+
                 sc.AddScore(1, 3);
                 sc.AddScore(2, 3);
                 sc.AddScore(3, 3);
